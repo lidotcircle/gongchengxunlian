9153545101f8b1655c9996e4b50595875af477a8
analyman <beam.analyman@gmail.com>
2020-09-26 15:31:33 +0800

    第一次作业

---
+++ src/app/pages/welcome/welcome.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { IonSlides } from '@ionic/angular';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.page.html',
+  styleUrls: ['./welcome.page.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class WelcomePage implements OnInit {
+  showSkip = true;
+  @ViewChild('slides', {static: false}) slides: IonSlides;
+
+  /** slides change */
+  onSlideWillChange(event) {
+    event.target.isEnd().then((end) => {
+      this.showSkip = !end;
+    });
+  }
+
+ constructor() {}
+
+  ngOnInit() {
+  }
+}
