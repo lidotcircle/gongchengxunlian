6f70b97e4ec8cbd046471a165174723e8bb828fc
analyman <beam.analyman@gmail.com>
2020-09-30 19:51:10 +0800

    save

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { LoginInfoService } from './../../shared/service/login-info.service';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(private logininfo: LoginInfoService) { }
 
   ngOnInit() {
   }
-
 }
+
